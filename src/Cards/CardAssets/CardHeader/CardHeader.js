c7dfd3b0e826274ce7e5c53e42ec4ef5b2228585
lukalomidzee <lukalomidze2000@gmail.com>
2025-04-05 13:03:50 +0400

    Change header icon

--- src/Cards/CardAssets/CardHeader/CardHeader.js
+++ src/Cards/CardAssets/CardHeader/CardHeader.js
@@ -1,19 +1,23 @@
-import './CardHeader.css'
-import BackButton from '../Button/BackButton';
+import "./CardHeader.css";
+import BackButton from "../Button/BackButton";
 
-function CardHeader(props){
+function CardHeader(props) {
+  let backButton = props.backButton ? (
+    <BackButton title="უკან" onClick={props.changeState} />
+  ) : null;
 
-    let backButton = props.backButton ? <BackButton title='უკან' onClick={props.changeState} /> : null
-    
-    let header = props.header ? <h1 id='card-header-paragraph' style={{color: "#074e66"}}>{props.header}</h1> : null
+  let header = props.header ? (
+    <h1 id="card-header-paragraph" style={{ color: "#074e66" }}>
+      {props.header}
+    </h1>
+  ) : null;
 
-    return (
-        <div className={`cardHeader ${props.classList || ''}`} >
-            {backButton}
-            {header}
-            
-        </div>
-    );
+  return (
+    <div className={`cardHeader ${props.classList || ""}`}>
+      {backButton}
+      {header}
+    </div>
+  );
 }
 
 export default CardHeader;
--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,17 +1,15 @@
-import './Header.css';
-import plateLogo from '../assets/icons/myplate-brand.svg';
+import "./Header.css";
+// import plateLogo from "../assets/icons/myplate-brand.svg";
 
-function Header(){
-    return (
-        <header className='header'>
-            <div>
-                <img src={plateLogo} className='plate-image' alt='plate-icon'/>
-            </div>
-            <div>
-                <p className='header-link'>MyPlate</p>
-            </div>
-        </header>
-    );
+function Header() {
+  return (
+    <header className="header">
+      <h1 style={{ color: "#074e66", fontSize: "1.1rem" }}>GNSPEN</h1>
+      <div>
+        <p className="header-link">MyPlate</p>
+      </div>
+    </header>
+  );
 }
 
 export default Header;
